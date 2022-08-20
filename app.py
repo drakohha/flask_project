fd602056184c7092fbc1a2e8af600c415fceeb4d
drakonha <drakohha@tut.by>
2022-08-20 19:34:19 +0300

    new final

--- app.py
+++ app.py
@@ -29,14 +29,49 @@ def index():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.data).all()
+    articles = Article.query.order_by(Article.data.desc()).all()
     return render_template("posts.html", articles=articles)
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article= Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+@app.route('/posts/<int:id>/del')
+def posts_delete(id):
+    article= Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произощла ошибка"
 
 
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/posts/<int:id>/update',methods=['POST','GET'])
+def post_update(id):
+    article=Article.query.get(id)
+    if request.method=='POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        article = Article.query.get(id)
+        return render_template("post_update.html", article=article)
+
 
 
 @app.route('/create-article',methods=['POST','GET'])
